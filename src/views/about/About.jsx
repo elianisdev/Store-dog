2ea586e4664061013f4efda0836c4b669f7b4d13
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-01-10 15:04:18 -0500

    buscador de todos

--- src/views/about/About.jsx
+++ src/views/about/About.jsx
@@ -6,17 +6,17 @@ import CreateTodoButton from "../../components/todo/Button/CreateTodoButton";
 import TodoItem from "../../components/todo/Todo Item/TodoItem";
 
 const defaultTodos = [
-  { text: 'Estudiar React', completed: false },
-  { text: 'Estudiar condicionales', completed: true },
-  { text: 'Estudiar validaciones', completed: true },
-  { text: 'Ver television', completed: false },
-  { text: 'Sacar a Mia', completed: true} ,
-  { text: 'Escuchar podcats', completed: false }
+  {text: 'Estudiar React', completed: false},
+  {text: 'Estudiar condicionales', completed: true},
+  {text: 'Estudiar validaciones', completed: true},
+  {text: 'Ver television', completed: false},
+  {text: 'Sacar a Mia', completed: true},
+  {text: 'Escuchar podcats', completed: false}
 ]
 const About = () => {
   const [todos, setTodos] = useState(defaultTodos)
   const [searchValue, setSearchValue] = useState('');
-  const completedTodos = todos.filter(todo=>
+  const completedTodos = todos.filter(todo =>
     !!todo.completed).length;
   const totalTodos = todos.length;
 
@@ -27,18 +27,48 @@ const About = () => {
       return todo.text.includes(searchText)
     }
   )
+  const todoComplete = (text) => {
+    const newTodos = [...todos];
+    const todoIndex = newTodos.findIndex(
+      (todo) => todo.text == text
+    )
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos)
+  }
 
-  console.log('Los Usuarios buscan todos de ' + searchValue)
+
+  //let palabras = "murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco tigre jaguar leopardo oso lobo zorro mapache ardilla elefante rinoceronte hipopotamo cebra jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago flamenco pinguino tucan loro canario colibri buho aguila halcon paloma pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo leon jirafa murcielago caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco murcielago leon jirafa cebra elefante rinoceronte hipopotamo ardilla mapache zorro lobo oso puma jaguar tigre leopardo gato perro caballo vaca toro cerdo oveja cabra gallina pato ganso pavo paloma halcon aguila buho colibri canario loro tucan pinguino flamenco oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato leopardo tigre jaguar oso lobo zorro mapache ardilla cebra elefante rinoceronte hipopotamo jirafa leon murcielago pavo ganso pato gallina cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon paloma colibri canario loro tucan pinguino flamenco jaguar oso lobo zorro mapache ardilla hipopotamo rinoceronte elefante jirafa leon murcielago cabra oveja cerdo toro vaca caballo perro gato buho aguila halcon"
+
+ // let words = "murcielago";
+
+  //let coincidencias = cuantasVecesAparece(palabras, words)
+
+  //for(let i = 0; i < palabras.lenght; i++) {
+  //  if (palabras[i].toLowerCase() === "murcielago") words.push(i);
+ // }
+
+ // document.getElementById("resultado").innerHTML = words.length;
+
+  //function cuantasVecesAparece(murcielago, leon){
+    //let words = [];
+   // for(let i = 0; i < murcielago.length; i++){
+    //  if(murcielago[i].toLowerCase() === leon) words.push(i);
+    //}
+   // return words.length;
+ // }
+
+ // let coincidencias = cuantasVecesAparece("murcielago")
+  //document.getElementById("resultado").innerHTML = coincidencias
 
   return (
     <>
-     <TodoCounter
-     completado = {completedTodos}
-     total= {totalTodos}
-     />
+      <TodoCounter
+        completado={completedTodos}
+        total={totalTodos}
+      />
       <TodoSearch
-      searchValue={searchValue}
-      setSearchValue={setSearchValue}
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
       />
       <TodoList>
         {searchedTodos.map((todo, index) => (
@@ -46,12 +76,14 @@ const About = () => {
             key={index}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => todoComplete(todo.text)}
 
           />
         ))}
       </TodoList>
 
-      <CreateTodoButton />
+
+      <CreateTodoButton/>
     </>
   );
 };
