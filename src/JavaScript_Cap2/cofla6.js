64498fd3e51c6c0450a5809f669bec372fbd0a18
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-03-16 22:15:25 -0500

    Junior a Mid

---
+++ src/JavaScript_Cap2/cofla6.js
@@ -0,0 +1,26 @@
+alumno = [{
+    nombre: "Cofla",
+    email: "cofla@mail.com",
+    materia: "Programación",
+},
+  {
+    nombre: "karen",
+    email: "karen@mail.com",
+    materia: "Programación",
+  },
+  {
+    nombre: "laura",
+    email: "laura@mail.com",
+    materia: "Programación",
+  },
+  {
+    nombre: "jorge",
+    email: "jorge@mail.com",
+    materia: "Programación",
+  },
+  {
+    nombre: "mia",
+    email: "mia@mail.com",
+    materia: "Programación",
+  }
+];
